c9ce7f31f3c7f41d48db21bdafe31d3a39e24788
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2023-12-03 11:34:46 +0300

    add box pokemon

--- models/User.js
+++ models/User.js
@@ -25,7 +25,7 @@ const UserSchema= new mongoose.Schema({
     },
     boss:{
         type:Array,
-        default:[{name:'BigDuck',url:"https://i.pinimg.com/564x/e5/83/b2/e583b27c52c5fe0813bdd140655cafb3.jpg",hpNow:10000,hpAll:10000}]
+        default:[{name:'BigDuck',url:"https://i.pinimg.com/564x/e5/83/b2/e583b27c52c5fe0813bdd140655cafb3.jpg",hpNow:10000,hpAll:10000},{name:'MonkeyBox',url:"https://static.wikia.nocookie.net/anime-characters-fight/images/0/0e/%D0%9F%D1%80%D0%B0%D0%B9%D0%BC%D1%8D%D0%B9%D0%BF%D0%90%D1%80%D1%82.png/revision/latest?cb=20201222113449&path-prefix=ru",hpNow:30000,hpAll:30000}]
     }
 
 },{
